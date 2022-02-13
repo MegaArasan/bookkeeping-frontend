c1b6f9e9bd03c9f4b063b31a2ff470f862b8b504
MegaArasan <megaarasan2000@gmail.com>
2022-02-13 23:22:53 +0530

    updated

--- src/components/Invoices/PaymentHistory.js
+++ src/components/Invoices/PaymentHistory.js
@@ -1,6 +1,13 @@
 import React from "react";
 import moment from "moment";
 import { toCommas } from "../../initialState.js";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const PaymentHistory = ({ paymentRecords }) => {
   return (
@@ -12,22 +19,28 @@ const PaymentHistory = ({ paymentRecords }) => {
         </label>
         <div className="tab-content">
           <div>
-            <table>
-              <tbody>
-                <tr>
-                  <th>Date Paid</th>
-                  <th>Amount Paid</th>
-                  <th>Payment Method</th>
-                </tr>
-                {paymentRecords?.map((record) => (
-                  <tr key={record._id}>
-                    <td>{moment(record.datePaid).format("MMMM Do YYYY")}</td>
-                    <td>{toCommas(record.amountPaid)}</td>
-                    <td>{record.paymentMethod}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date Paid</TableCell>
+                    <TableCell>Amount Paid</TableCell>
+                    <TableCell>Payment Method</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {paymentRecords?.map((record) => (
+                    <TableRow key={record._id}>
+                      <TableCell>
+                        {moment(record.datePaid).format("MMMM Do YYYY")}
+                      </TableCell>
+                      <TableCell>{toCommas(record.amountPaid)}</TableCell>
+                      <TableCell>{record.paymentMethod}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </div>
         </div>
       </div>
